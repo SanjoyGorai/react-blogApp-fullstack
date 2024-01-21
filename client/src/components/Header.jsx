ba0c87495d30755e425a344968adf0df386a502e
Mr. Sanjoy <--add>
2024-01-21 20:01:28 +0530

    basic header is created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
-import {Navbar} from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
+import { Link } from 'react-router-dom'
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <Navbar>Nav </Navbar>
+    <Navbar className='border-b-2'>
+      <Link to='/' className='self-center whitespace-nowrap text-sm 
+      sm:text-xl font-semibold dark:text-white' >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
+         via-purple-500 to-pink-500 text-white rounded-lg'>Sanjoy</span>
+        Blog
+      </Link>
+
+      <form>
+
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          rightIcon={AiOutlineSearch}
+          className='hidden lg:inline'
+        />
+      </form>
+      <Button className='w-12 h-10 ' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex gap-2'>
+        <Button className='w-12 h-10 sm:inline ' color='gray' pill><FaMoon /> </Button>
+        <Link to='/signin' className=''>
+          <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+        </Link>
+      </div>
+
+    </Navbar>
   )
 }
 
