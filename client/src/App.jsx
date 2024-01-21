5d908d57d8c9f7f728bf00d57745722d9eb8d88a
Mr. Sanjoy <--add>
2024-01-21 18:56:21 +0530

    app created

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-600'>App
+    <h1 className='font-bold text-green-400'>ferfer</h1></div>
+  )
+}
+
+export default App
