5b4036a67e5f3b76060a37391e72e738b6f77e9f
Mr. Sanjoy <--add>
2024-01-21 19:01:01 +0530

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <div className='bg-red-600'>App
-    <h1 className='font-bold text-green-400'>ferfer</h1></div>
+      <h1 className='font-bold text-green-400'>ferfer</h1>
+      </div>
   )
 }
 
