df31f97935984bd03f0ce6785caeb35c23a4468c
Mr. Sanjoy <--add>
2024-01-22 10:00:45 +0530

    header section is created

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
+
+  const path = useLocation()
+
   return (
     <Navbar className='border-b-2'>
       <Link to='/' className='self-center whitespace-nowrap text-sm 
@@ -31,7 +34,23 @@ const Header = () => {
         <Link to='/signin' className=''>
           <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
         </Link>
+        <Navbar.Toggle />
       </div>
+
+      <Navbar.Collapse>
+        <Navbar.Link active={path == '/'} as={'div'}>
+          <Link to={'/'}> Home
+          </Link>
+        </Navbar.Link>
+        <Navbar.Link active={path == '/about'} as={'div'}>
+          <Link to={'/about'}> About
+          </Link>
+        </Navbar.Link >
+        <Navbar.Link active={path == '/projects'} as={'div'}>
+          <Link to={'/projects'}> Projects
+          </Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
 
     </Navbar>
   )
